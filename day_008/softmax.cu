676b2c91395ee1333ec798e8acad2e349c9349e8
Abdellah <abdellah.oumida@student.fr>
2025-02-04 23:49:36 +0100

    added a simple test and use cas for the function

--- day_008/softmax.cu
+++ day_008/softmax.cu
@@ -26,3 +26,61 @@ __global__ void normalizeKernel(float* output, int size, float sum) {
     }
 }
 
+int main() {
+    int size = VECTOR_SIZE;
+    size_t vectorSize = size * sizeof(float);
+
+    float *h_input = (float*)malloc(vectorSize);
+    float *h_output = (float*)malloc(vectorSize);
+
+    srand(1234); 
+    for (int i = 0; i < size; i++) {
+        h_input[i] = static_cast<float>(rand()) / RAND_MAX; 
+    }
+
+    
+    printf("Input vector (first 5 elements):\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%f ", h_input[i]);
+    }
+    printf("\n");
+
+    float *d_input, *d_output;
+    CUDA_CHECK(cudaMalloc(&d_input, vectorSize));
+    CUDA_CHECK(cudaMalloc(&d_output, vectorSize));
+    CUDA_CHECK(cudaMemcpy(d_input, h_input, vectorSize, cudaMemcpyHostToDevice));
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
+
+    softmaxKernel<<<blocksPerGrid, threadsPerBlock>>>(d_input, d_output, size);
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
+    float h_sum = 0.0f;
+    CUDA_CHECK(cudaMemcpy(h_output, d_output, vectorSize, cudaMemcpyDeviceToHost));
+    for (int i = 0; i < size; i++) {
+        h_sum += h_output[i];
+    }
+
+    printf("Sum of exponentials: %f\n", h_sum);
+
+    normalizeKernel<<<blocksPerGrid, threadsPerBlock>>>(d_output, size, h_sum);
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
+
+    CUDA_CHECK(cudaMemcpy(h_output, d_output, vectorSize, cudaMemcpyDeviceToHost));
+
+    printf("Softmax probabilities for the first 5 elements:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%f ", h_output[i]);
+    }
+    printf("\n");
+
+    CUDA_CHECK(cudaFree(d_input));
+    CUDA_CHECK(cudaFree(d_output));
+    free(h_input);
+    free(h_output);
+
+    return 0;
+}
+
