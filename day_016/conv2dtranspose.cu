892ca95d45e6a40e70479a5f59a0d01bd83a83b3
Meeex2 <heroarcher459@gmail.com>
2025-02-13 01:19:21 +0100

    Add test function for 2D transposed convolution with CPU-GPU comparison

--- day_016/conv2dtranspose.cu
+++ day_016/conv2dtranspose.cu
@@ -90,3 +90,70 @@ void conv_transpose2d_cpu(
     }
 }
 
+void test_conv_transpose2d() {
+    
+    int batch_size = 1;
+    int in_channels = 1;
+    int out_channels = 1;
+    int H_in = 3, W_in = 3;
+    int kernel_h = 3, kernel_w = 3;
+    int stride_h = 2, stride_w = 2;
+    int padding_h = 1, padding_w = 1;
+    int H_out = (H_in - 1) * stride_h + kernel_h - 2 * padding_h;
+    int W_out = (W_in - 1) * stride_w + kernel_w - 2 * padding_w;
+    
+    std::vector<float> h_input(batch_size * in_channels * H_in * W_in, 1.0f);
+    std::vector<float> h_weight(out_channels * in_channels * kernel_h * kernel_w, 1.0f);
+    std::vector<float> h_output_cpu(batch_size * out_channels * H_out * W_out);
+    std::vector<float> h_output_gpu(h_output_cpu.size());
+    
+    float *d_input, *d_weight, *d_output;
+    cudaMalloc(&d_input, h_input.size() * sizeof(float));
+    cudaMalloc(&d_weight, h_weight.size() * sizeof(float));
+    cudaMalloc(&d_output, h_output_gpu.size() * sizeof(float));
+    
+    cudaMemcpy(d_input, h_input.data(), h_input.size() * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_weight, h_weight.data(), h_weight.size() * sizeof(float), cudaMemcpyHostToDevice);
+    
+    conv_transpose2d_cpu(
+        h_input.data(), h_weight.data(), h_output_cpu.data(),
+        batch_size, in_channels, out_channels,
+        H_in, W_in, H_out, W_out,
+        kernel_h, kernel_w,
+        stride_h, stride_w,
+        padding_h, padding_w
+    );
+    
+    int total_output = batch_size * out_channels * H_out * W_out;
+    int block_size = 256;
+    int grid_size = (total_output + block_size - 1) / block_size;
+    
+    conv_transpose2d_kernel<<<grid_size, block_size>>>(
+        d_input, d_weight, d_output,
+        batch_size, in_channels, out_channels,
+        H_in, W_in, H_out, W_out,
+        kernel_h, kernel_w,
+        stride_h, stride_w,
+        padding_h, padding_w
+    );
+    cudaMemcpy(h_output_gpu.data(), d_output, h_output_gpu.size() * sizeof(float), cudaMemcpyDeviceToHost);
+    
+    float tolerance = 1e-4;
+    for (size_t i = 0; i < h_output_cpu.size(); ++i) {
+        if (fabs(h_output_cpu[i] - h_output_gpu[i]) > tolerance) {
+            std::cerr << "Mismatch at index " << i 
+                      << ": CPU=" << h_output_cpu[i] 
+                      << ", GPU=" << h_output_gpu[i] << std::endl;
+            return;
+        }
+    }
+    std::cout << "Test passed!" << std::endl;
+    
+    cudaFree(d_input);
+    cudaFree(d_weight);
+    cudaFree(d_output);
+}
+int main() {
+    test_conv_transpose2d();
+    return 0;
+}
